83a9651db211e80c6b40bcd22a3518ccfab996e5
Wilson Sy <sy.wilson@protonmail.com>
2018-11-27 21:48:40 -0500

    Beautify js and some bug fixes
    Bugs squashed:
     - fixed variable declarations
    
    Cleaned up comments as well.

--- Othello/function.js
+++ Othello/function.js
@@ -1,27 +1,25 @@
-window.clicked=false;
-window.started=false;
-window.turn = 1;
+let clicked = false;
+let started = false;
+let turn = 1;
 
-function start(){
+function start() {
+    if (!started) {
+        let grid = "<table><tr>";
 
-    if (!started){
-        grid = "<table><tr>";
-
-
-        for(x=1;x<=8;x++){
-            for(y=1;y<=8;y++){
-                grid = grid + `<td class='grey' onmouseover='mouseon(${x},${y})' onmouseout='mouseoff(${x},${y})' 
+        for (let x = 1; x <= 8; x++) {
+            for (let y = 1; y <= 8; y++) {
+                grid += `<td class='grey' onmouseover='mouseon(${x},${y})' onmouseout='mouseoff(${x},${y})' 
                 onclick = 'mouseclick(${x},${y})' id = 'row${x},column${y}'></td>`;
             }
-            if (x!=8){
-                grid = grid+`</tr id = 'row${x}'><tr>`;
+            if (x != 8) {
+                grid += `</tr id = 'row${x}'><tr>`;
             }
         }
 
-        grid = grid + "</tr></table>";
-        
+        grid += "</tr></table>";
+
         document.getElementById("demo").innerHTML = grid;
-        started=true;
+        started = true;
 
         document.getElementById(`row4,column4`).className = "white";
         document.getElementById(`row5,column5`).className = "white";
@@ -30,25 +28,29 @@ function start(){
     }
 }
 
+function mouseon(a, b) {
+    if (clicked) {
 
-function mouseon(a,b){
-    if (clicked){
-
-    }else{
-    element = document.getElementById(`row${a},column${b}`);
-    element.classList.remove("white_hover");
-    console.log(a+'\n'+b);
+    } else {
+        let element = document.getElementById(`row${a},column${b}`);
+        element.classList.add("white_hover");
+        console.log(a + '\n' + b);
     }
 }
 
-function mouseclick(a,b){
-    // if (check(a,b)){
-	
-    // }
+function mouseoff(a, b) {
+    if (clicked) {
 
-    left(a,b,turn);
-    right(a,b,turn);
+    } else {
+        let element = document.getElementById(`row${a},column${b}`);
+        element.classList.remove("white_hover");
+        console.log(a + '\n' + b);
+    }
+}
 
+function mouseclick(a, b) {
+    left(a, b, turn);
+    right(a, b, turn);
     // clicked = true;
-    console.log(a+'\n'+b);
+    console.log(a + '\n' + b);
 }
--- Othello/mechanics.js
+++ Othello/mechanics.js
@@ -1,41 +1,32 @@
-// a=row
-// b=column
-// c=turn
-// odd=white,even=black
-//
-//f,g are temporary variables to be used in the function
-//x,y are temporary variables to be used in the for statements
-//
-//
-a=4;
-b=6;
-c=1;
+let a = 4; // row
+let b = 6; // column
+let c = 1; // turn (odd=white, even=black)
 
-function left(a,b,c){
-    line=[];
-    c=cword(c);
+function left(a, b, c) {
+    let line = [];
+    c = cword(c);
 
-    for (y=b;y>=1;y--){
-        element = document.getElementById(`row${a},column${y}`);
+    for (y = b; y >= 1; y--) {
+        let element = document.getElementById(`row${a},column${y}`);
         element.classList.remove("white_hover");
         element.classList.remove("black_hover");
-        temp = element.className;
+        let temp = element.className;
         line.push(temp);
     }
 
-    if (!checkarray(line)){
+    if (!checkarray(line)) {
         //console.log(line);
-    }else {
-        num = calculatearray(line,c);
+    } else {
+        let num = calculatearray(line, c);
         console.log(num);
     }
 
-    if (num !=false){
-        for (y=b;y>=1;y--){
-            if (num>=0){
+    if (num != false) {
+        for (y = b; y >= 1; y--) {
+            if (num >= 0) {
                 document.getElementById(`row${a},column${y}`).className = c;
                 num--;
-            }else{
+            } else {
                 break;
             }
         }
@@ -44,78 +35,26 @@ function left(a,b,c){
     document.getElementById("temp2").innerHTML = line;
 }
 
-
-/*
-function right(a,b,c){
-    line=[];
-    c=cword(c);
-///////////////////////////fix////////////////////////////////////
-    for (y=b;y>=1;y--){
-        element = document.getElementById(`row${a},column${y}`);
-        element.classList.remove("white_hover");
-        element.classList.remove("black_hover");
-        temp = element.className;
-        line.push(temp);
-    }
-
-    if (!checkarray(line)){
-        //console.log(line);
-    }else {
-        num = calculatearray(line,c);
-        console.log(num);
-    }
-
-    if (num !=false){
-        for (y=b;y>=1;y--){
-            if (c=="black"){
-                if (num>=0){
-                    document.getElementById(`row${a},column${y}`).className = "black";
-                    num--;
-                }else{
-                    break;
-                }
-            }else if (c=="white"){
-                if (num>=0){
-                    document.getElementById(`row${a},column${y}`).className = "white";
-                    num--;
-                }else{
-                    break;
-                }
-            }
-        }
-        turn++;
-    }
-}
-*/
-
-
-
-
-
-
 //array stuff
-
-function checkarray(line){
-
-    if (line[0]!='grey'){
+function checkarray(line) {
+    if (line[0] != 'grey') {
         return false;
-    }
-    else {
+    } else {
         return true;
     }
 }
 
-function calculatearray(line,c){
-    for(k=1;k<=line.length-1;k++){
-        if (line[k]=='grey'){
+function calculatearray(line, c) {
+    for (let k = 1; k <= line.length - 1; k++) {
+        if (line[k] == 'grey') {
             return false;
-        }else{
-            if (c=="black"){
-                if (line[k]=='white' && line[k+1]=='black'){
+        } else {
+            if (c == "black") {
+                if (line[k] == 'white' && line[k + 1] == 'black') {
                     return k;
                 }
-            }else if (c=="white"){
-                if (line[k]=='black' && line[k+1]=='white'){
+            } else if (c == "white") {
+                if (line[k] == 'black' && line[k + 1] == 'white') {
                     return k;
                 }
             }
@@ -123,12 +62,12 @@ function calculatearray(line,c){
     }
 }
 
-function cword(c){
-    if (c%2==1){
+function cword(c) {
+    if (c % 2 == 1) {
         return "white";
-    }else if(c%2==0){
+    } else if (c % 2 == 0) {
         return "black";
-    }else {
+    } else {
         return "panic";
     }
 }
