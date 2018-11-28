6759454cd7700cc1e4f66ac39d15c8a196b47d3b
Wei <599164@pdsb.org>
2018-11-28 09:51:05 -0500

    bug fixes and efficentized it

--- Othello/function.js
+++ Othello/function.js
@@ -31,7 +31,7 @@ function start() {
 
 function mouseon(a, b) {
     if (clicked) {
-
+        //do nothing
     } else {
         let element = document.getElementById(`row${a},column${b}`);
         element.classList.add("white_hover");
@@ -44,7 +44,7 @@ function mouseoff(a, b) {
     } else {
         let element = document.getElementById(`row${a},column${b}`);
         element.classList.remove("white_hover");
-        console.log(a + '\n' + b);
+        //console.log(a + '\n' + b);
     }
 }
 
@@ -54,6 +54,7 @@ function mouseclick(a, b) {
     // clicked = true;
     if (addturn) {
         turn++;
+        addturn = false;
     }
-    console.log(a + '\n' + b);
+    //console.log(a + '\n' + b);
 }
--- Othello/mechanics.js
+++ Othello/mechanics.js
@@ -1,6 +1,6 @@
-let a = 4; // row
-let b = 6; // column
-let c = 1; // turn (odd=white, even=black)
+// a,x,j are row variables
+// b,y,k are column variables
+// turn (odd=white, even=black)
 
 function left(a, b, c) {
     let line = [];
@@ -10,49 +10,38 @@ function left(a, b, c) {
         let element = document.getElementById(`row${a},column${y}`);
         element.classList.remove("white_hover");
         element.classList.remove("black_hover");
-        let temp = element.className;
-        line.push(temp);
+        line.push(element.className);
     }
 
-    let num = legalmove(line, cwords);
+    console.log(line)
 
+    let num = calculatearray(line, c);
+
+    console.log(num);
+    
     if (num != false) {
         for (let y = b; y >= 1; y--) {
             let element = document.getElementById(`row${a},column${y}`).className;
             if (element == cword(c)) {
-                console.log("break");
+                //console.log("break");
                 break;
             } else if (element == cword(c + 1)) {
-                console.log("set");
+                //console.log("set");
                 document.getElementById(`row${a},column${y}`).className = cword(c);
             } else if (element == "grey") {
-                console.log("grey");
+                //console.log("grey");
             } else {
-                console.log("error");
+                //console.log("error");
             }
-            /*
-            if (temp<=num){
-				if (temp == 0){
-					temp++;
-					continue;
-				}else{
-					temp++;
-					document.getElementById(`row${a},column${y}`).className = c;
-				}
-			}else{
-				break;
-            }
-            */
         }
-        let addturn = true;
+        addturn = true;
     }
-    //document.getElementById("temp1").innerHTML = num;
-    document.getElementById("temp2").innerHTML = line;
+
 }
 
 function right(a, b, c) {
     let line = [];
-    let cwords = cword(c);
+    cwords = cword(c);
 
     for (let y = b; y <= 8; y++) {
         let element = document.getElementById(`row${a},column${y}`);
@@ -62,27 +51,29 @@ function right(a, b, c) {
         line.push(temp);
     }
 
-    let num = legalmove(line, c);
+    console.log(line)
+
+    let num = calculatearray(line, c);
+
+    console.log(num);
 
     if (num != false) {
-        let temp = 0;
         for (let y = b; y <= 8; y++) {
-            if (temp <= num) {
-                if (temp == 0) {
-                    temp++;
-                    continue;
-                } else {
-                    temp++;
-                    document.getElementById(`row${a},column${y}`).className = c;
-                }
-            } else {
+            let element = document.getElementById(`row${a},column${y}`).className;
+            if (element == cword(c)) {
+                //console.log("break");
                 break;
+            } else if (element == cword(c + 1)) {
+                //console.log("set");
+                document.getElementById(`row${a},column${y}`).className = cword(c);
+            } else if (element == "grey") {
+                //console.log("grey");
+            } else {
+                //console.log("error");
             }
         }
-        let addturn = true;
+        addturn = true;
     }
-    document.getElementById("temp1").innerHTML = num;
-    document.getElementById("temp2").innerHTML = line;
 }
 
 ///////////////////////////fix////////////////////////////////////
@@ -101,7 +92,7 @@ function up(a,b,c){
         line.push(temp);
     }
 
-    num = legalmove(line,c);
+    num = calculatearray(line,c);
 
     if (num !=false){
         for (x=a;x>=1;x--){
@@ -137,7 +128,7 @@ function down(a,b,c){
         line.push(temp);
     }
 
-    num = legalmove(line,c);
+    num = calculatearray(line,c);
 
     if (num !=false){
         for (x=a;x<=8;x++){
@@ -175,37 +166,24 @@ function cword(c) {
     }
 }
 
-//array stuff
-function legalmove(line, c) {
 
-    if (!checkarray(line)) {} else {
-        return calculatearray(line, c);
-    }
-}
-
-function checkarray(line) {
-
-    if (line[0] != 'grey') {
-        return false;
-    } else {
-        return true;
-    }
-}
-
+//check if the line is legal or not
 function calculatearray(line, c) {
-    for (let k = 1; k <= line.length - 1; k++) {
-        if (line[k] == 'grey') {
+    for (k=1;k<=line.length-1;k++){
+        if (line[0] != 'grey'){
             return false;
-        } else {
-            if (c == "black") {
-                if (line[k] == 'white' && line[k + 1] == 'black') {
-                    return k;
-                }
-            } else if (c == "white") {
-                if (line[k] == 'black' && line[k + 1] == 'white') {
-                    return k;
-                }
-            }
+        }else if (line[k]=='grey'){
+            return false;
+        }else if (line[k-1]==cword(c+1) && line[k]==cword(c)){
+            return true;
         }
     }
+}
+
+
+
+//printing
+
+function printtogrid(j,k){
+
 }
