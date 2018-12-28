ffef5d292c2b04197da50d7599def673742154a6
Wilson Sy <sy.wilson@protonmail.com>
2018-12-28 15:17:32 -0500

    Visual ship placement for 1 player
    also:
     - resize some elements
     - add help text
     - make grid generator flexible
     - rewrite grid system using custom attributes
     - create 'settings.js' to contain all game globals + methods

--- battleships/game.js
+++ battleships/game.js
@@ -1,22 +1,75 @@
-function genGrid() {
-    // deletes the launch button and its containing row
+/* globals game, grey */
+
+$(document).ready(function() {
+    $('#info').hide();
+    $('#launch').click(initialize);
+});
+
+function initialize() {
     $('#launch').remove();
 
-    // creates an x by y table
+    // creates a grid where bottom-left cell is 0,0
     let grid = "<table id='grid'>";
-    for (let y = 9; y >= 0; y--) {
-        grid += `<tr class='y${y}'>`;
-        for (let x = 0; x <= 9; x++) {
-            grid += `<td class='x${x}'></td>`;
+    for (let y = game.field.height - 1; y >= 0; y--) {
+        grid += `<tr>`;
+        for (let x = 0; x < game.field.width; x++) {
+            grid += `<td data-x='${x}' data-y='${y}'></td>`;
         }
         grid += "</tr>";
     }
     grid += "</table>";
     $('#battlefield').append(grid);
-    console.log("Battlefield generated.");
+    $('#info').show();
+
+    // check for click
+    $('td').click(clickDetect);
+
+    // check for keypress (indicates rotate)
+    $(document).keypress(e => {
+        if (['w', 'a', 's', 'd', ].includes(e.key)) game.cursor.rotate(e.key);
+    });
+
+    // start placement of ships
+    game.cursor.action = 'placing';
+    game.currentPlayer = 1;
+    game.iterateShips = 0;
+    $('#info-main').text(`Player ${game.currentPlayer}, place your ships!`);
+    // if (game.player2.isHuman) placement(2);
 }
 
-$(document).ready(function () {
-    $('#launch').click(genGrid);
-    //$('#grid').mouseover(gridDetect);
-});
+function clickDetect() {
+    console.log("Click found at " + $(this).attr('data-x') + "," + $(this).attr('data-y'));
+    switch (game.cursor.action) {
+        case 'placing':
+            placement(parseInt($(this).attr('data-x')), parseInt($(this).attr('data-y')));
+            break;
+        case 'aiming':
+            // shoot(parseInt($(this).attr('data-x')), parseInt($(this).attr('data-y')));
+            break;
+    }
+}
+
+function placement(x, y) {
+    if (game.cursor.isClear(x, y, game.ships[game.listShips()[game.iterateShips]].length, game.cursor.direction)) {
+        // if the selected tiles are clear, fill them and move onto placing next ship
+        $('#info-small').css('color', 'black').empty();
+        $('[selected]').css({
+            'background-color': grey,
+            'cursor': 'default',
+        });
+        console.log(`${game.listShips()[game.iterateShips]} placed!`);
+        game.iterateShips++;
+    } else {
+        // if selected tiles are invalid, update help message
+        $('#info-small').css('color', 'red').text("Invalid ship position!");
+        console.log("Invalid ship position!");
+    }
+    // deselect all tiles
+    $('[selected]').attr('selected', false);
+    // end placement phase after last ship is placed
+    if (game.iterateShips >= game.listShips().length) {
+        $('#info-main').empty();
+        $('#info-small').empty();
+        game.cursor.action = false;
+    }
+}
---
+++ battleships/settings.js
@@ -0,0 +1,74 @@
+let grey = "rgb(128, 128, 128)";
+
+let game = {
+    turn: 1,
+    currentPlayer: 1, // 1 or 2
+    listShips: function() {
+        return Object.keys(this.ships);
+    },
+    ships: {
+        carrier: {
+            length: 5,
+        },
+        battleship: {
+            length: 4,
+        },
+        cruiser: {
+            length: 3,
+        },
+        submarine: {
+            length: 3,
+        },
+        destroyer: {
+            length: 2,
+        },
+    },
+    player1: {
+        name: "",
+        isHuman: true, // should always be true
+    },
+    player2: {
+        name: "",
+        isHuman: true,
+    },
+    field: {
+        height: 10,
+        width: 10,
+    },
+    cursor: {
+        action: false, // 3 possible values - placing, aiming, false
+        direction: 'vertical', // 2 possible values - vertical, horizontal
+        rotate: key => {
+            switch (key) {
+                case 'w':
+                case 's':
+                    game.cursor.direction = 'vertical';
+                    break;
+                case 'a':
+                case 'd':
+                    game.cursor.direction = 'horizontal';
+                    break;
+            }
+            console.log("Cursor rotated.");
+        },
+        isClear: (x, y, distance, direction) => {
+            switch (direction) {
+                case 'vertical':
+                    if (y - distance + 1 < 0) return false;
+                    for (let i = y; i > y - distance; i--) {
+                        if ($(`[data-x=${x}][data-y=${i}]`).css('background-color') == grey) return false;
+                        else $(`[data-x=${x}][data-y=${i}]`).attr('selected', true);
+                    }
+                    break;
+                case 'horizontal':
+                    if (x + distance > game.field.width) return false;
+                    for (let i = x; i < x + distance; i++) {
+                        if ($(`[data-x=${i}][data-y=${y}]`).css('background-color') == grey) return false;
+                        else $(`[data-x=${i}][data-y=${y}]`).attr('selected', true);
+                    }
+                    break;
+            }
+            return true;
+        },
+    },
+};
