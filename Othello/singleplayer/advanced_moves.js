e5c35ed57eb16c9cb531de8e49c0a3d3507aaebd
Daniel <45181932+The-Daniel@users.noreply.github.com>
2019-01-06 17:23:17 -0500

    Ideas

--- Othello/singleplayer/advanced_moves.js
+++ Othello/singleplayer/advanced_moves.js
@@ -2,12 +2,12 @@
 
 function bestmove(){
 /*
-This "thinks ahead" two steps, then choose most valuable move?
+"think ahead" two steps, then choose most valuable move?
+
+tips and trick?
 */
 
 // we want to get pieces to be stable?
-
-
 
 
 
@@ -17,5 +17,28 @@ function stable(){
 
 
 
-    
+
+}
+
+function earlygame(){
+/*first 10 moves
+limit moves opponent can make
+less tiles, more maneuverability
+make opponent give up tiles surrounding corners
+*/
+
+}
+
+function midgame(){
+/*
+get corners
+get stable tiles
+*/
+}
+
+function endgame(){
+
+/*last 20 moves
+count number of pieces opponent will get vs. you, then move (think ahead)
+*/
 }
