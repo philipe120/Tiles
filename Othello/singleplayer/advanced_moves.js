846ae6372229b429a0353071200b958462a60981
Wei <599164@pdsb.org>
2019-01-07 10:59:01 -0500

    idea

--- Othello/singleplayer/advanced_moves.js
+++ Othello/singleplayer/advanced_moves.js
@@ -1,44 +1,29 @@
+f_move1 = [];
+f_move2 = [];
+f_move3 = [];
+
+
 // store stuff for hard difficulty
 
 function bestmove(){
 /*
-"think ahead" two steps, then choose most valuable move?
+"think ahead" four steps, then choose most valuable move
 
 tips and trick?
 */
 
 // we want to get pieces to be stable?
+analyse();
+}
 
 
+
+function analyse(){
 
 }
 
-function stable(){
+function cornertrap(){
 
 
 
-
-}
-
-function earlygame(){
-/*first 10 moves
-limit moves opponent can make
-less tiles, more maneuverability
-make opponent give up tiles surrounding corners
-*/
-
-}
-
-function midgame(){
-/*
-get corners
-get stable tiles
-*/
-}
-
-function endgame(){
-
-/*last 20 moves
-count number of pieces opponent will get vs. you, then move (think ahead)
-*/
 }
