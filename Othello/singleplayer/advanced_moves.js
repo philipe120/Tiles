fb40b4a26a555d7b2180cc9831d1efe520e6fff3
Wei <599164@pdsb.org>
2019-01-08 09:46:11 -0500

    made simulated grid

--- Othello/singleplayer/advanced_moves.js
+++ Othello/singleplayer/advanced_moves.js
@@ -1,7 +1,8 @@
 f_move1 = [];
 f_move2 = [];
 f_move3 = [];
-
+simgrid = [];
+//simgrid = [["grey","grey",etc.],["grey","grey",etc.],etc.]
 
 // store stuff for hard difficulty
 
@@ -13,9 +14,26 @@ tips and trick?
 */
 
 // we want to get pieces to be stable?
-analyse();
+create_simgrid()
 }
 
+function create_simgrid(){
+    simgrid = [];
+    for (let x = 1; x <= 8; x++){
+        simgrid[x] = [];
+        for (let y = 1; y <= 8; y++){
+            let element = document.getElementById(`row${x},column${y}`);
+            element.classList.remove("white_hover");
+            element.classList.remove("black_hover");
+            simgrid[x][y] = element.className;
+        }
+    }
+    console.log(simgrid);
+}
+
+function analyse_simgrid(){
+
+}
 
 
 function analyse(){
